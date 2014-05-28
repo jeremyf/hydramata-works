8bccc5bda3e88d2f950341a1b70b54a540c40917
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-28 16:56:59 -0400

    Adding fast spec helper
    To run specs fast I'm taking notes from Jim Weirich

---
+++ spec/fast_helper.rb
@@ -0,0 +1,11 @@
+require 'rspec/given'
+Dir[File.expand_path("../../app/*", __FILE__)].each do |dir|
+  $LOAD_PATH << dir
+end
+$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+
+unless defined?(require_dependency)
+  def require_dependency(*files)
+    require *files
+  end
+end
