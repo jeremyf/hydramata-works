3713781237ef2352892b9b3c118e893243f86ba8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-04 12:07:42 -0400

    Moving Presented models to Presenter directory

--- lib/hydramata/work/engine.rb
+++ lib/hydramata/work/engine.rb
@@ -5,9 +5,11 @@ module Hydramata
 
       initializer 'hydramata_work.initializers' do |app|
         app.config.paths.add 'app/renderers', eager_load: true
+        app.config.paths.add 'app/presenters', eager_load: true
         app.config.paths.add 'app/parsers', eager_load: true
         app.config.autoload_paths += %W(
           #{config.root}/app/renderers
+          #{config.root}/app/presenters
           #{config.root}/app/parsers
         )
       end
