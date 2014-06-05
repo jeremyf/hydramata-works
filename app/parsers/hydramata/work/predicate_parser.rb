dd0041039d460db877152910844cc8ed7e9f6801
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-05 13:52:45 -0400

    Fleshing out the PredicateParser interaface

--- app/parsers/hydramata/work/predicate_parser.rb
+++ app/parsers/hydramata/work/predicate_parser.rb
@@ -11,7 +11,23 @@ module Hydramata
       module_function
 
       def call(options = {}, &block)
+        parser = parser_for(options)
+        value = options.fetch(:value)
+        parser.call(value, &block)
       end
+
+      def parser_for(options = {})
+        parser_finder = options.fetch(:parser_finder) { default_parser_finder }
+        parser_finder.call(options)
+      end
+
+      def default_parser_finder
+        ->(options) do
+          require 'hydramata/work/predicate_parsers/simple_parser'
+          PredicateParsers::SimpleParser
+        end
+      end
+      private_class_method :default_parser_finder
     end
   end
 end
