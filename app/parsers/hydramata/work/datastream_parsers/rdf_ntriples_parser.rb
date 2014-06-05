7ba93c54799422325dde53f98691cac97ffb82dd
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-05 14:44:18 -0400

    Adding RdfNtriplesParser collaborator options

--- app/parsers/hydramata/work/datastream_parsers/rdf_ntriples_parser.rb
+++ app/parsers/hydramata/work/datastream_parsers/rdf_ntriples_parser.rb
@@ -1,16 +1,15 @@
-require 'rdf'
 module Hydramata
   module Work
     module DatastreamParsers
       class RdfNtriplesParser
-        def self.call(data, &block)
-          new.call(data, &block)
+        def self.call(data, collaborators = {}, &block)
+          new(collaborators).call(data, &block)
         end
 
-        def initialize
-          @graph = default_graph
-          @reader = default_reader
-          @pattern = default_pattern
+        def initialize(collaborators = {})
+          @graph = collaborators.fetch(:graph) { default_graph }
+          @reader = collaborators.fetch(:reader) { default_reader }
+          @pattern = collaborators.fetch(:pattern) { default_pattern }
         end
 
         def call(data, &block)
@@ -30,14 +29,17 @@ module Hydramata
         private :graph, :reader, :pattern
 
         def default_graph
+          require 'rdf'
           RDF::Graph.new
         end
 
         def default_reader
+          require 'rdf'
           RDF::Reader.for(:ntriples)
         end
 
         def default_pattern
+          require 'rdf'
           RDF::Query::Pattern.from([:s, :p, :o])
         end
       end
