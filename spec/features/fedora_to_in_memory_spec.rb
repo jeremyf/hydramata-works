bb1313fd86d29a8263ec97189ca9dda079ee7bcb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-05 14:07:47 -0400

    Adding a Fedora test of property values
    @WIP - The specs are broken.

--- spec/features/fedora_to_in_memory_spec.rb
+++ spec/features/fedora_to_in_memory_spec.rb
@@ -93,6 +93,25 @@ module Hydramata
           )
         end
       end
+
+      it 'should parse the fedora object to retrieve the depositor' do
+        VCR.use_cassette('fedora-object', record: :none) do
+          work_wrangler.call(pid, with_datastreams: true)
+          expect {|b| entity.properties.each(&b) }.
+          to yield_successive_args(
+            Property.new(predicate: 'depositor', value: 'username-1'),
+            Property.new(predicate: 'http://purl.org/dc/terms/created', value: Date.new(2014, 6, 2)),
+            Property.new(predicate: 'http://purl.org/dc/terms/language', value: 'English'),
+            Property.new(predicate: 'http://purl.org/dc/terms/publisher', value: 'University of Notre Dame'),
+            Property.new(predicate: 'http://purl.org/dc/terms/title', value: 'Title 1'),
+            Property.new(predicate: 'http://purl.org/dc/terms/dateSubmitted', value: Date.new(2014, 6, 2)),
+            Property.new(predicate: 'http://purl.org/dc/terms/modified', value: Date.new(2014, 6, 2)),
+            Property.new(predicate: 'http://purl.org/dc/terms/rights', value: 'Attribution 3.0 United States'),
+            Property.new(predicate: 'http://purl.org/dc/terms/creator', value: 'Creator Name1'),
+            Property.new(predicate: 'http://purl.org/dc/terms/description', value: 'Hello World')
+          )
+        end
+      end
     end
   end
 end
