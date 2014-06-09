2283a6ce55461c620d774fa1e42504f4c9a788fa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-09 14:06:18 -0400

    Adding :view_chain options for BasePresenter
    With this addition we can now attempt to render partials with greater
    degrees of specificity.
    
    For instance, we could indicate that all properties attempt view
    lookups in the following order:
    
    `:work_type/:fieldset/:property`
    `:work_type/:property`
    `:property`
    
    To consider, but not accounted for:
    
    `:work_type/:fieldset/:property`
    `:fieldset/:property`
    `:work_type/:property`
    `:property`

--- app/presenters/hydramata/work/base_presenter.rb
+++ app/presenters/hydramata/work/base_presenter.rb
@@ -4,17 +4,19 @@ module Hydramata
     # Responsible for coordinating the rendering of an in-memory data structure
     # object to an output buffer.
     class BasePresenter < SimpleDelegator
-      attr_reader :presentation_context, :translator
+      attr_reader :presentation_context, :translator, :view_chain, :template_missing_error
       def initialize(object, collaborators = {})
         __setobj__(object)
         @presentation_context = collaborators.fetch(:presentation_context) { default_presentation_context }
         @translator = collaborators.fetch(:translator) { default_translator }
+        @view_chain = collaborators.fetch(:view_chain) { default_view_chain }
+        @template_missing_error = collaborators.fetch(:template_missing_exception) { default_template_missing_exception }
       end
 
       def render(options = {})
         template = options.fetch(:template)
-        rendering_options = rendering_options_for(partial_name, options)
-        template.render(rendering_options)
+        rendering_options = rendering_options_for(options)
+        render_with_diminishing_specificity(template, rendering_options)
       end
 
       def inspect
@@ -36,14 +38,37 @@ module Hydramata
 
       private
 
-      def rendering_options_for(partial_name, options = {})
-        returning_options = { partial: partial_name, object: self }
+      def render_with_diminishing_specificity(template, rendering_options)
+        view_depth = view_chain.size
+        (0...view_depth).each do |index|
+          begin
+            partial_prefix = view_chain[0..(view_chain.size - index - 1)]
+            rendering_options[:partial] = partial_name(partial_prefix)
+            template.render(rendering_options)
+          # By using the splat operator I am allowing multiple exceptions to
+          # be caught and pass to the next rendering context.
+          rescue *template_missing_error
+            next
+          end
+        end
+
+        # Our last resort! If this fails, we want it to bubble up.
+        rendering_options[:partial] = partial_name
+        template.render(rendering_options)
+      end
+
+      def default_view_chain
+        []
+      end
+
+      def rendering_options_for(options = {})
+        returning_options = { object: self }
         returning_options[:locals] = options[:locals] if options.key?(:locals)
         returning_options
       end
 
-      def partial_name
-        File.join('hydramata/work', view_path_slug_for_object, presentation_context.to_s)
+      def partial_name(current_view_chain = [])
+        File.join('hydramata/work', view_path_slug_for_object, current_view_chain.join("/"), presentation_context.to_s)
       end
 
       def default_presentation_context
@@ -65,6 +90,13 @@ module Hydramata
       def default_translator
         require 'i18n'
         I18n
+      end
+
+      # Because actually testing this is somewhat of a nightmare given the
+      # 5+ parameters that are required when instantiating this exception.
+      def default_template_missing_exception
+        require 'action_view/template/error'
+        ActionView::MissingTemplate
       end
     end
   end
--- spec/presenters/hydramata/work/base_presenter_spec.rb
+++ spec/presenters/hydramata/work/base_presenter_spec.rb
@@ -1,4 +1,12 @@
 require 'fast_helper'
+
+unless defined?(ActionView)
+  module ActionView
+    class MissingTemplate < RuntimeError
+    end
+  end
+end
+
 require 'hydramata/work/base_presenter'
 
 module Hydramata
@@ -37,6 +45,39 @@ module Hydramata
         expect(subject.dom_class).to eq('hello-world')
       end
 
+      context '#render' do
+        let(:template) { double('Template', render: true)}
+
+        it 'should delegate render to the template' do
+          subject.render(template: template)
+          expect(template).to have_received(:render).with(partial: 'hydramata/work/base/show', object: subject)
+        end
+
+        it 'should handle a view_chain rendering in less and less specificity' do
+          subject = described_class.new(object, translator: translator, view_chain: ['article', 'required'], presentation_context: 'show', template_missing_exception: [RuntimeError, NoMethodError])
+          expect(template).to receive(:render).
+            with(partial: 'hydramata/work/base/article/required/show', object: subject).
+            ordered.
+            and_raise(NoMethodError)
+          expect(template).to receive(:render).
+            with(partial: 'hydramata/work/base/article/show', object: subject).
+            ordered.
+            and_raise(RuntimeError)
+          expect(template).to receive(:render).
+            with(partial: 'hydramata/work/base/show', object: subject).and_return('YES').
+            ordered
+          expect(subject.render(template: template)).to eq('YES')
+        end
+
+        it 'should raise exception if we are rendering as general as possible' do
+          subject = described_class.new(object, translator: translator, presentation_context: 'show')
+          expect(template).to receive(:render).
+            with(partial: 'hydramata/work/base/show', object: subject).and_return('YES').
+            and_raise(RuntimeError)
+          expect { subject.render(template: template) }.
+            to raise_error(RuntimeError)
+        end
+      end
     end
   end
 end
