68876c656bab5abc3bc53e7b989754b2377ad48b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-18 14:09:45 -0400

    Adding translation services specs for propertyset
    This is still not in the ideal format, but by creating the specs I can
    more readily shift things around.

--- spec/features/translation_services_spec.rb
+++ spec/features/translation_services_spec.rb
@@ -1,10 +1,11 @@
 require 'feature_helper'
+require 'hydramata/work/conversions/property_set'
 
 module Hydramata
   module Work
     describe 'translation services' do
+      include Conversions
       let(:entity) { Entity.new.tap {|e| e.work_type = work_type } }
-
 
       context 'for entities' do
         let(:presenter) { EntityPresenter.new(entity: entity, presentation_structure: nil) }
@@ -18,6 +19,24 @@ module Hydramata
           let(:work_type) { 'Non-Translated Work Type' }
           it 'translates its :name' do
             expect(presenter.t(:name)).to eq('Non-Translated Work Type')
+          end
+        end
+      end
+
+      context 'for property_sets' do
+        let(:presenter) { FieldsetPresenter.new(entity: entity, fieldset: fieldset) }
+        context 'with translated work type' do
+          let(:fieldset) { PropertySet(identity: 'Translated Property Set') }
+          let(:work_type) { 'Translated Work Type' }
+          it 'translates its :name' do
+            expect(presenter.t(:name)).to eq('I Am a Translated Property Set!')
+          end
+        end
+        context 'without translated work type' do
+          let(:fieldset) { PropertySet(identity: 'Non-Translated Property Set') }
+          let(:work_type) { 'Non-Translated Work Type' }
+          it 'translates its :name' do
+            expect(presenter.t(:name)).to eq('Non-Translated Property Set')
           end
         end
       end
