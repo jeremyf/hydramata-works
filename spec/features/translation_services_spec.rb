c5bc0245a7d63119a8dc817bbfaf232b7b3fd24f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-18 15:29:03 -0400

    Improving legibility of translation files
    I want the context to be first, translated makes more sense as a
    suffix instead of a prefix.

--- spec/features/translation_services_spec.rb
+++ spec/features/translation_services_spec.rb
@@ -11,7 +11,7 @@ module Hydramata
       context 'for entities' do
         let(:presenter) { EntityPresenter.new(entity: entity, presentation_structure: nil) }
         context 'with existing work type translations' do
-          let(:work_type) { 'Translated Work Type' }
+          let(:work_type) { 'Work Type Translated' }
           it 'translates its :name from the lookup table' do
             expect(presenter.t(:name)).to eq('I Am a Translated Work Type!')
           end
@@ -27,14 +27,14 @@ module Hydramata
       context 'for property_sets' do
         let(:presenter) { FieldsetPresenter.new(entity: entity, fieldset: fieldset) }
         context 'with existing property set and work type translations' do
-          let(:fieldset) { PropertySet(identity: 'Translated Property Set') }
-          let(:work_type) { 'Translated Work Type' }
+          let(:fieldset) { PropertySet(identity: 'Property Set Translated') }
+          let(:work_type) { 'Work Type Translated' }
           it 'translates its :name from the lookup table' do
             expect(presenter.t(:name)).to eq('I Am a Translated Property Set for a Translated Work Type!')
           end
         end
         context 'with existing property set but not work type translations' do
-          let(:fieldset) { PropertySet(identity: 'Translated Property Set') }
+          let(:fieldset) { PropertySet(identity: 'Property Set Translated') }
           let(:work_type) { 'Non-Translated Work Type' }
           it 'translates its :name' do
             expect(presenter.t(:name)).to eq('I Am a Translated Property Set with a non-Translated Work Type!')
@@ -53,16 +53,16 @@ module Hydramata
       context 'for properts' do
         let(:presenter) { PropertyPresenter.new(entity: entity, fieldset: fieldset, property: property) }
         context 'with existing property and work type translations' do
-          let(:fieldset) { PropertySet(identity: 'Translated Property Set') }
-          let(:property) { Property('Translated Property') }
-          let(:work_type) { 'Translated Work Type' }
+          let(:fieldset) { PropertySet(identity: 'Property Set Translated') }
+          let(:property) { Property('Property Translated') }
+          let(:work_type) { 'Work Type Translated' }
           it 'translates its :name from the lookup table' do
             expect(presenter.t(:name)).to eq('I Am a Translated Property for a Translated Work Type!')
           end
         end
         context 'with existing property but not property set translations' do
           let(:fieldset) { PropertySet(identity: 'Non-Translated Property Set') }
-          let(:property) { Property('Translated Property') }
+          let(:property) { Property('Property Translated') }
           let(:work_type) { 'Non-Translated Work Type' }
           it 'translates its :name' do
             expect(presenter.t(:name)).to eq('I Am a Translated Property with a non-Translated Work Type!')
