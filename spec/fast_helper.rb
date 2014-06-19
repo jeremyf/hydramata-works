8a9c1d4626bbe96e037d0e361bd3c9fa06c0cc97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-19 15:18:39 -0400

    Updating fast helper
    Less chatter

--- spec/fast_helper.rb
+++ spec/fast_helper.rb
@@ -1,6 +1,5 @@
 unless defined?(Rails)
   Dir[File.expand_path('../../app/*', __FILE__)].each do |dir|
-    puts dir
     $LOAD_PATH << dir
   end
   $LOAD_PATH << File.expand_path('../../lib', __FILE__)
