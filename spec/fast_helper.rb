e10390cb83794ec5547b15795e9ade5ef48b180b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-19 14:41:09 -0400

    Tweaking fast_helper.rb
    Don't load if Rails is defined

--- spec/fast_helper.rb
+++ spec/fast_helper.rb
@@ -1,11 +1,13 @@
-require 'rspec'
-Dir[File.expand_path('../../app/*', __FILE__)].each do |dir|
-  $LOAD_PATH << dir
-end
-$LOAD_PATH << File.expand_path('../../lib', __FILE__)
+unless defined?(Rails)
+  Dir[File.expand_path('../../app/*', __FILE__)].each do |dir|
+    puts dir
+    $LOAD_PATH << dir
+  end
+  $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 
-unless defined?(require_dependency)
-  def require_dependency(*files)
-    require *files
+  unless defined?(require_dependency)
+    def require_dependency(*files)
+      require *files
+    end
   end
 end
