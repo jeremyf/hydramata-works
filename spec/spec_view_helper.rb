06835eb8a862084044f0d6067bcbdaa209f56418
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-20 14:49:16 -0400

    Replacing spec_slow_helper with spec_view_helper
    By leveraging specific requires instead of the shotgun method, the
    specs run almost a full 2 seconds faster, dropping from 2.8 seconds to
    0.78 seconds.
    
    Using the spec_slow_helper:
    `Finished in 0.34274 seconds (files took 2.49 seconds to load)`
    
    Using the spec_view_helper:
    `Finished in 0.05838 seconds (files took 0.73006 seconds to load)`

---
+++ spec/spec_view_helper.rb
@@ -0,0 +1,12 @@
+# I'm deliberately not including this, as I was hoping to keep the views tested
+# in extreme isolation.
+# require 'spec_fast_helper'
+
+require 'active_support'
+require 'action_view'
+require 'action_controller'
+
+require 'rspec/rails'
+require 'rspec-html-matchers'
+
+ActionController::Base.prepend_view_path(File.expand_path('../../app/views', __FILE__))
