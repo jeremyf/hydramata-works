dac0c84ec9bf272c13eab5bdedb8031a1dc480c9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-24 13:20:19 -0400

    Adding PresentationStructure conversion

--- spec/models/hydramata/work/presentation_structure_spec.rb
+++ spec/models/hydramata/work/presentation_structure_spec.rb
@@ -6,6 +6,15 @@ module Hydramata
     describe PresentationStructure do
       subject { described_class.new }
       let(:fieldset) { [:required, [:title]] }
+
+      context '.build' do
+        let(:object) { double('Object with predicate sets', predicate_sets: ['predicate_1', 'predicate_2']) }
+        it 'should transform an entities predicate_sets to fieldsets' do
+          structure = described_class.build_from(object)
+          expect(structure.fieldsets).to eq(object.predicate_sets)
+        end
+      end
+
       context '#fieldsets' do
         it 'can be appended' do
           expect { subject.fieldsets << fieldset }.
