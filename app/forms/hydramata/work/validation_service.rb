3a8255194f894c24584ac2da6f6bd422ad5d2624
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-25 16:28:26 -0400

    Implementing validation service end to end
    This required several adjustments:
    
    * Adding #to_sym to DataDefinition interface
    * Adding non-isolated validation service test
    
    @TODO - The ValidationService object has a lot of knowledge about the
    inner-workings of the collaborating entity

--- app/forms/hydramata/work/validation_service.rb
+++ app/forms/hydramata/work/validation_service.rb
@@ -3,19 +3,22 @@ require 'active_support/core_ext/array/wrap'
 module Hydramata
   module Work
     class ValidationService
-      def self.call(context)
-        new(context).call
+      def self.call(entity)
+        new(entity).call
       end
 
-      attr_reader :context, :validator_namespaces
-      def initialize(context, collaborators = {})
-        @context = context
+      attr_reader :entity, :validator_namespaces
+      def initialize(entity, collaborators = {})
+        @entity = entity
         self.validator_namespaces = collaborators.fetch(:validator_namespaces) { default_validator_namespaces }
       end
 
       def call
-        each_predicate_with_validation_options do |predicate, validations|
-          validations.each do |validator_name, options|
+        # @TODO - This is an awful lot of information about the validation
+        # structure.
+        entity.properties.each do |property|
+          predicate = property.predicate
+          predicate.validations.each do |validator_name, options|
             validate(predicate, validator_name, options)
           end
         end
@@ -24,19 +27,12 @@ module Hydramata
 
       private
 
-      def each_predicate_with_validation_options
-        context.predicates_with_validations.each do |predicate, validations|
-          yield(predicate, validations)
-        end
-      end
-
-
       def validate(predicate, validator_name, options)
         return unless options
         validator = validator_for(validator_name)
         validation_options = parse_validates_options(options)
         validation_options[:attributes] = predicate
-        validator.new(validation_options).validate(context)
+        validator.new(validation_options).validate(entity)
       end
 
       def validator_for(validator_name)
