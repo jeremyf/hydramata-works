260d7c5a8985ce02cdb8ab9a295d68cc0372c9f9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-07-08 14:42:41 -0400

    Adjusting spec structure for clarity
    As I prepare for a demo, I want the feature specs to be very well
    defined.

--- spec/features/instantiate_entity_from_persisted_work_type_spec.rb
+++ spec/features/instantiate_entity_from_persisted_work_type_spec.rb
@@ -2,7 +2,7 @@ require 'spec_slow_helper'
 
 module Hydramata
   module Works
-    describe 'instantiate entity from persisted work type' do
+    describe 'instantiating an entity from a persisted work type' do
       before do
         @article = Hydramata::Works::WorkTypes::Storage.create!(identity: 'article', name_for_application_usage: 'article')
         @predicate_set = Hydramata::Works::PredicateSets::Storage.create!(identity: 'required', work_type: @article, presentation_sequence: 1, name_for_application_usage: 'required')
@@ -12,14 +12,23 @@ module Hydramata
         @predicate_set.predicate_presentation_sequences.create!(presentation_sequence: 2, predicate: @alternate_predicate)
       end
 
+      let(:property) { Property.new(predicate: 'title', values: ['Hello', 'World']) }
       let(:work) do
         Entity.new(work_type: 'article') do |work|
-          work.properties << Property.new(predicate: 'http://purl.org/dc/terms/dc_title', values: ["Hello", "World"])
+          work.properties << property
         end
       end
 
-      it 'should have #fieldsets' do
-        expect(work.work_type.fieldsets.size).to eq(1)
+      it 'should have a #work_type' do
+        expect(work.work_type).to eq(@article.to_work_type)
+      end
+
+      it 'should retrieve a property via a key' do
+        expect(work.properties[:title].values).to eq(['Hello', 'World'])
+      end
+
+      it 'should retrieve a property with values that were not explicitly set' do
+        expect(work.properties[:alternate].values).to eq([])
       end
     end
   end
