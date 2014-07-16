994379c43f4187f0ca937e859991a80fbc320ce0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-07-16 15:29:56 -0400

    Fixing build
    And making sure that the in-memory Work's identity is updated

--- spec/persisters/hydramata/works/database_persister_spec.rb
+++ spec/persisters/hydramata/works/database_persister_spec.rb
@@ -12,6 +12,7 @@ module Hydramata
       let(:work) { double('Work', work_type: 'Article', properties: [property_1, property_2])}
 
       it 'call down to the underlying storage' do
+        expect(work).to receive(:identity=).with(pid)
         described_class.call(work: work, storage_service: storage_service, pid_minting_service: pid_minting_service)
         expect(storage_service).to have_received(:call).with(pid: pid, work_type: 'Article', properties: {'Title' => ['Hello World'], 'Description' => ['A Brief Description']})
       end
