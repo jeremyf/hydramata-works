99f9e6c414404a3aaff58f17f14f39ea3ca3b42e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-07-31 15:26:39 -0400

    Moving app/wranglers to app/services
    The wrangler directory is a weird concept.

--- lib/hydramata/works/engine.rb
+++ lib/hydramata/works/engine.rb
@@ -18,14 +18,12 @@ module Hydramata
         app.config.paths.add 'app/persisters', eager_load: true
         app.config.paths.add 'app/presenters', eager_load: true
         app.config.paths.add 'app/parsers', eager_load: true
-        app.config.paths.add 'app/wranglers', eager_load: true
         app.config.autoload_paths += %W(
           #{config.root}/app/renderers
           #{config.root}/app/forms
           #{config.root}/app/persisters
           #{config.root}/app/presenters
           #{config.root}/app/parsers
-          #{config.root}/app/wranglers
         )
       end
     end
