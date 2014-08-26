81b46fbac6a9747375bb1efb949bcbc32a2f6739
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-26 09:55:26 -0400

    Removing namespace constraint on ServiceMethods
    In theory, other people could implement non-namespaced classes to
    override the namespaced classes. Which means all kinds of
    configuration options.

--- app/services/hydramata/works/service_methods.rb
+++ app/services/hydramata/works/service_methods.rb
@@ -3,6 +3,8 @@ module Hydramata
     # Responsible for being a collection of service methods that are useful
     # for negotiating between a context (i.e. a Controller request ) and the
     # domain model.
+    #
+    # This module defines the public API for interacting with Hydramata::Works.
     module ServiceMethods
       # @param :context [#current_user]
       # @param :work_type [String]
@@ -11,11 +13,11 @@ module Hydramata
       # @yieldparam work [WorkForm]
       # @return [WorkForm]
       def new_work_for(context, work_type, attributes = {}, &block)
-        work = Hydramata::Works::Work.new(work_type: work_type)
+        work = Work.new(work_type: work_type)
         ApplyUserInputToWork.call(work: work, attributes: attributes) if attributes.present?
-        presenter = Hydramata::Works::WorkPresenter.new(work: work, presentation_context: :new)
+        presenter = WorkPresenter.new(work: work, presentation_context: :new)
         presenter.actions << { name: :create }
-        Hydramata::Works::WorkForm.new(presenter, &block)
+        WorkForm.new(presenter, &block)
       end
     end
   end
