18193a8ece7eb056a43c51b80a3c046c47d8f453
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-26 09:41:22 -0400

    Adding comments for ServiceMethods

--- app/services/hydramata/works/service_methods.rb
+++ app/services/hydramata/works/service_methods.rb
@@ -1,10 +1,16 @@
 module Hydramata
   module Works
     # Responsible for being a collection of service methods that are useful
-    # for negotiating between a context (i.e. a Controller request ) and the 
+    # for negotiating between a context (i.e. a Controller request ) and the
     # domain model.
     module ServiceMethods
-      def new_work_for(context, work_type, attributes, &block)
+      # @param :context [#current_user]
+      # @param :work_type [String]
+      # @param :attributes [Hash]
+      # @yield [work]
+      # @yieldparam work [WorkForm]
+      # @return [WorkForm]
+      def new_work_for(context, work_type, attributes = {}, &block)
         work = Hydramata::Works::Work.new(work_type: work_type)
         ApplyUserInputToWork.call(work: work, attributes: attributes) if attributes.present?
         presenter = Hydramata::Works::WorkPresenter.new(work: work, presentation_context: :new)
