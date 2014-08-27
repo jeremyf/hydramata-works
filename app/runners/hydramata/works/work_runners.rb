5ed63d47c50cee61c0fddd9249676d1fbb17e77f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-27 10:58:19 -0400

    Adding the runners
    These were extracted from the hydra_connect_demo rails application.
    
    The extraction was because I wasn't comfortable using the stubbed
    behavior of a gem inside a separate application. In other words,
    testing the boundaries was a bit odd.

---
+++ app/runners/hydramata/works/work_runners.rb
@@ -0,0 +1,34 @@
+require 'hydramata/core/runner'
+
+module Hydramata
+  module Works
+    module WorkRunners
+
+      class New < Hydramata::Core::Runner
+        def run(work_type, attributes)
+          work = services.new_work_for(self, work_type, attributes)
+          callback(:success, work)
+        end
+      end
+
+      class AvailableType < Hydramata::Core::Runner
+        def run
+          work_types = services.available_work_types(self)
+          callback(:success, work_types)
+        end
+      end
+
+      class Create < Hydramata::Core::Runner
+        def run(work_type, attributes)
+          work = services.new_work_for(self, work_type, attributes)
+          if services.save_work(work)
+            callback(:success, work)
+          else
+            callback(:failure, work)
+          end
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/runners/hydramata/works/work_runners_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_fast_helper'
+require 'hydramata/works/work_runners'
+require 'hydramata/works/stub_callback'
+
+module Hydramata
+  module Works
+    module WorkRunners
+      describe New do
+
+        Given(:callback) { StubCallback.new }
+        Given(:callback_config) { callback.configure(:success) }
+        Given(:context) { double('Context', services: services) }
+        Given(:returning_object) { double('Returning Object') }
+
+        describe New do
+          Given(:work_type) { 'article' }
+          Given(:attributes) { {} }
+          Given(:runner) { described_class.new(context, &callback_config) }
+          Given(:services) { double('Services', new_work_for: returning_object)}
+          When(:result) { runner.run(work_type, attributes) }
+          Then { expect(result).to eq([returning_object]) }
+          And { expect(callback.invoked).to eq([:success, returning_object]) }
+          And { expect(services).to have_received(:new_work_for).with(runner, work_type, attributes) }
+        end
+
+        describe AvailableType do
+          Given(:runner) { described_class.new(context, &callback_config) }
+          Given(:returning_object) { double('Returning Object') }
+          Given(:services) { double('Services', available_work_types: returning_object)}
+          When(:result) { runner.run }
+          Then { expect(result).to eq([returning_object]) }
+          And { expect(callback.invoked).to eq([:success, returning_object]) }
+          And { expect(services).to have_received(:available_work_types).with(runner) }
+        end
+
+        describe New do
+          Given(:work_type) { 'article' }
+          Given(:attributes) { {} }
+          Given(:runner) { described_class.new(context, &callback_config) }
+          Given(:services) { double('Services', new_work_for: returning_object)}
+          When(:result) { runner.run(work_type, attributes) }
+          Then { expect(result).to eq([returning_object]) }
+          And { expect(callback.invoked).to eq([:success, returning_object]) }
+          And { expect(services).to have_received(:new_work_for).with(runner, work_type, attributes) }
+        end
+
+      end
+    end
+  end
+end
