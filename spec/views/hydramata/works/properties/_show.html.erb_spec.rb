620cee1ef594f1f65e21940d34be80b76e31a912
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-28 09:55:26 -0400

    Adding aria-labelledby attributes to html rendering.
    Based on https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_aria-labelledby_attribute

--- spec/views/hydramata/works/properties/_show.html.erb_spec.rb
+++ spec/views/hydramata/works/properties/_show.html.erb_spec.rb
@@ -6,17 +6,23 @@ require 'spec_view_helper'
 describe 'hydramata/works/properties/_show.html.erb', type: :view do
   let(:value1) { double('Value 1', render: '<dd class="my-dom-class value">value1</dd>'.html_safe ) }
   let(:value2) { double('Value 2', render: '<dd class="my-dom-class value">value2</dd>'.html_safe ) }
-  let(:object) { double('Object', t: true, dom_class: 'my-dom-class', values: [value1, value2]) }
+  let(:object) { double('Object', predicate: 'title', t: true, dom_class: 'my-dom-class', values: [value1, value2]) }
 
   it 'renders the object and fieldsets' do
-    expect(object).to receive(:container_content_tag_attributes).with(class: 'label').and_return(class: ['my-dom-class', 'label'])
-    expect(object).to receive(:container_content_tag_attributes).with(class: 'value').and_return(class: ['my-dom-class', 'value']).at_least(1).times
+    expect(object).
+      to receive(:container_content_tag_attributes).
+      with(id: "label_for_work_title", class: 'label').
+      and_return(id: "label_for_work_title", class: ['my-dom-class', 'label'])
+    expect(object).
+      to receive(:container_content_tag_attributes).
+      with(:class=>"value", "aria-labelledby"=>"label_for_work_title").
+      and_return(:class => ['my-dom-class', 'value'], "aria-labelledby"=>"label_for_work_title").at_least(1).times
     expect(object).to receive(:t).with(:name).and_return('Label')
     render partial: 'hydramata/works/properties/show', object: object
 
-    expect(rendered).to have_tag('.my-dom-class.label', text: 'Label')
-    expect(rendered).to have_tag('.my-dom-class.value', text: 'value1')
-    expect(rendered).to have_tag('.my-dom-class.value', text: 'value2')
+    expect(rendered).to have_tag('#label_for_work_title.my-dom-class.label', text: 'Label')
+    expect(rendered).to have_tag('.my-dom-class.value[aria-labelledby="label_for_work_title"]', text: 'value1')
+    expect(rendered).to have_tag('.my-dom-class.value[aria-labelledby="label_for_work_title"]', text: 'value2')
 
   end
 end
