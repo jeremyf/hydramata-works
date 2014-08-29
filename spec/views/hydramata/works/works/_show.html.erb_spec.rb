e79850a81fd60005008b0ba1893264b819f91fe7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-29 09:02:08 -0400

    Adding action rendering to works/_show

--- spec/views/hydramata/works/works/_show.html.erb_spec.rb
+++ spec/views/hydramata/works/works/_show.html.erb_spec.rb
@@ -5,20 +5,25 @@ require 'spec_view_helper'
 # in the example context
 describe 'hydramata/works/works/_show.html.erb', type: :view do
   let(:dom_attributes) { { class: ['work', 'my-dom-class'] } }
-  let(:object) { double('Object', fieldsets: [fieldset1, fieldset2], container_content_tag_attributes: dom_attributes) }
+  let(:object) { double('Object', actions: [action], fieldsets: [fieldset1, fieldset2], container_content_tag_attributes: dom_attributes) }
 
   # A short circuit as the render does not normally
   let(:fieldset1) { double('Fieldset', render: '<div class="set1">Fieldset 1</div>'.html_safe) }
   let(:fieldset2) { double('Fieldset', render: '<div class="set2">Fieldset 2</div>'.html_safe) }
+  let(:action) { double('Action', render: '<div id="action_1">Action 1</div>'.html_safe) }
 
   it 'renders the object and fieldsets' do
     render partial: 'hydramata/works/works/show', object: object
 
     expect(fieldset1).to have_received(:render).with(template: view)
     expect(fieldset2).to have_received(:render).with(template: view)
+    expect(action).to have_received(:render).with(template: view)
+
     expect(rendered).to have_tag('article.work.my-dom-class') do
       with_tag('.set1', text: 'Fieldset 1')
       with_tag('.set2', text: 'Fieldset 2')
+      with_tag('.actions #action_1', text: 'Action 1')
     end
   end
+
 end
