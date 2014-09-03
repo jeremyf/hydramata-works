2d066668ca19713671d0f65a0a847eb44ab0b0cc
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-09-03 10:58:29 -0400

    Adding DatabaseStorage#find_or_create_by_pid
    This method is rather large and may need to be extracted into a
    service class. There is plenty of logic occuring, and it needs to
    consider how the data is updated.

--- app/models/hydramata/works/works/database_storage.rb
+++ app/models/hydramata/works/works/database_storage.rb
@@ -6,7 +6,25 @@ module Hydramata
     module Works
       class DatabaseStorage < ActiveRecord::Base
         self.table_name = :hydramata_works_works
+        self.primary_key = :pid
         serialize :properties, Hash
+
+        def self.find_or_create_by_pid(attributes = {})
+          pid = attributes.fetch(:pid)
+          work = where(pid: pid).first || new(pid: pid)
+          work.work_type = attributes[:work_type] if attributes.key?(:work_type)
+          if attributes.key?(:properties)
+            work.properties ||= {}
+            attributes[:properties].each do |key ,value|
+              if value
+                work.properties[key] = value
+              else
+                work.properties.delete(key)
+              end
+            end
+          end
+          work.save
+        end
 
         def to_work
           Work.new(identity: pid, work_type: work_type, state: state) do |work|
