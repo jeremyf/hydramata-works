71d2dc5c4dbb30a295c2161229335b87f20ea673
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-09-10 11:14:25 -0400

    Tidying up ValuePresenterFinder behavior

--- app/presenters/hydramata/works/value_presenter_finder.rb
+++ app/presenters/hydramata/works/value_presenter_finder.rb
@@ -3,11 +3,19 @@ module Hydramata
     module ValuePresenterFinder
       module_function
       def call(predicate, options)
+        klass =
         if predicate.respond_to?(:value_presenter_class_name) && predicate.value_presenter_class_name
-          "Hydramata::Works::#{predicate.value_presenter_class_name}".constantize.new(options)
+          begin
+            predicate.value_presenter_class_name.constantize
+          rescue NameError
+            Hydramata::Works.const_get(predicate.value_presenter_class_name)
+          rescue NameError
+            ValuePresenter
+          end
         else
-          ValuePresenter.new(options)
+          ValuePresenter
         end
+        klass.new(options)
       end
     end
   end
