cb20bdd4e434da62f5663a4397034109e4d2c100
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-10-21 10:09:33 -0400

    Adding URL to Engine guide

--- lib/hydramata-works.rb
+++ lib/hydramata-works.rb
@@ -8,13 +8,19 @@ module Hydramata
   # * Rendering/Output Buffer
   module Works
     module_function
+    # Instead of prefering the isolate namespace, I'm explicitly declaring some
+    # of the behavior.
+    #
+    # @see http://crypt.codemancers.com/posts/2013-09-22-isolate-namespace-in-rails-engines/
     def table_name_prefix
       'hydramata_works_'
     end
 
     # Because I am not using isolate_namespace for Hydramata::Works::Engine
     # I need this for the application router to find the appropriate routes.
+    #
     # @api private
+    # @see http://crypt.codemancers.com/posts/2013-09-22-isolate-namespace-in-rails-engines/
     def use_relative_model_naming?
       true
     end
